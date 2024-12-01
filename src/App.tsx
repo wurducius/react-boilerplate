a70d791a994f5032232f23aa3fc97ad63fcd5917
Jakub Elias <elias@acema.cz>
2024-12-01 20:27:37 +0100

    Add mui, axios, helmet, router, bundle analyzer, cross-env, rimraf and sharp-cli

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,42 @@
-import React from "react"
-import logo from "./logo.svg"
-import { Counter } from "./features/counter/Counter"
+import React, { useEffect } from "react"
 import "./App.css"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import axios from "axios"
+import { Button } from "@mui/material"
 
 function App() {
+  useEffect(() => {
+    axios
+      .get("/user?ID=12345")
+      .then(function (response) {
+        // handle success
+        console.log(response)
+      })
+      .catch(function (error) {
+        // handle error
+        console.log(error)
+      })
+      .finally(function () {
+        // always executed
+      })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a className="App-link" href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">
-            React
-          </a>
-          <span>, </span>
-          <a className="App-link" href="https://redux.js.org/" target="_blank" rel="noopener noreferrer">
-            Redux
-          </a>
-          <span>, </span>
-          <a className="App-link" href="https://redux-toolkit.js.org/" target="_blank" rel="noopener noreferrer">
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a className="App-link" href="https://react-redux.js.org/" target="_blank" rel="noopener noreferrer">
-            React Redux
-          </a>
-        </span>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/x"} element={<>X</>} />
+          <Route
+            path={"/"}
+            element={
+              <>
+                INDEX
+                <Button variant="contained">Hello world</Button>
+              </>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,14 +4,20 @@ import { Provider } from "react-redux"
 import { store } from "./app/store"
 import App from "./App"
 import "./index.css"
+import { Helmet, HelmetProvider } from "react-helmet-async"
 
 const container = document.getElementById("root")!
 const root = createRoot(container)
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <HelmetProvider>
+      <Provider store={store}>
+        <Helmet>
+          <title>Hello World</title>
+        </Helmet>
+        <App />
+      </Provider>
+    </HelmetProvider>
   </React.StrictMode>,
 )
